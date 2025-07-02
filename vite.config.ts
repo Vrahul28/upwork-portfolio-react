fbe1568343e1b0ae8585f28682d0b0df47233da3
91887 <vrahul2828@gmail.com>
2025-07-02 16:01:49 +0530

    Fix: Use npx for esbuild and vite to avoid permission issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: 'https://vrahul28.github.io/upwork-portfolio-react/',
+  base: './',
   plugins: [
     react(),
     runtimeErrorOverlay(),
